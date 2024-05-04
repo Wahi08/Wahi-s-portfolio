4244c54c990919a7b8cc26751de063c78669d27d
Wahi08 <nwahidah0808@gmail.com>
2024-05-04 13:59:49 +0800

    Initial commit with project setup

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
